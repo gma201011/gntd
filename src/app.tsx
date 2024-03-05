8f657da439032541e7c448778c28f6774381a20b
tony.chang <tony.chang@vpon.com>
2024-03-05 22:45:30 +0800

    feat: add date-picker component

--- src/app.tsx
+++ src/app.tsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import Alert, { AlertType } from "./components/Alert/alert";
+import DatePicker from "./components/DatePicker/datePicker";
 import Input from "./components/Input/input";
 
 const App = (props: any) => {
@@ -9,11 +10,7 @@ const App = (props: any) => {
   }
   return (
     <div style={{ margin: "30px" }}>
-      <Input value={value} onChange={(e) => {setValue(e.target.value)}}></Input>
-      <Input style={{ width: "300px" }} />
-      <Input size='lg' style={{ width: "300px" }} defaultValue={"defaultValue"} prepend={"https://"} />
-      <Input size='sm' style={{ width: "300px" }} append={".com"} />
-      <Input disabled style={{ width: "300px" }} defaultValue={"disabled"} />
+      <DatePicker />
     </div>
   );
 };
---
+++ src/components/DatePicker/datePicker.tsx
@@ -0,0 +1,100 @@
+import React, { useState } from "react";
+import Icon from "../Icon/icon";
+
+export enum ButtonSize {
+  Large = "lg",
+  Small = "sm",
+}
+
+export enum ButtonType {
+  Primary = "primary",
+  Default = "default",
+  Danger = "danger",
+  Link = "link",
+}
+
+interface DatePickerProps {
+  value?: Date,
+  onChange?: (date: Date) => void,
+}
+
+const DatePicker: React.FC<DatePickerProps> = (props) => {
+  const { value = new Date(), onChange } = props;
+  const [date, setDate] = useState(value);
+
+  const monthNames = [
+    "一月",
+    "二月",
+    "三月",
+    "四月",
+    "五月",
+    "六月",
+    "七月",
+    "八月",
+    "九月",
+    "十月",
+    "十一月",
+    "十二月"
+  ];
+
+
+  const handlePreviousMonth = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() - 1, 1));
+  }
+
+  const handleNextMonth = () => {
+    setDate(new Date(date.getFullYear(), date.getMonth() + 1, 1));
+  }
+
+  const renderDate = () => {
+    const currentMonthDate = new Date(date.getFullYear(), date.getMonth() + 1, 0).getDate();
+    const firstWeekDay = new Date(date.getFullYear(), date.getMonth(), 1).getDay();
+
+    const monthDate = [];
+
+    for (let i = 0; i < currentMonthDate; i++) {
+      const clickHandler = onChange?.bind(null, new Date(date.getFullYear(), date.getMonth(), i + 1));
+      if (i === date.getDate()) {
+        monthDate.push(<div key={`date-${i + 1}`} className="day selected" onClick={clickHandler}>{i + 1}</div>);
+      } else {
+        monthDate.push(<div key={`date-${i + 1}`} className="day" onClick={clickHandler}>{i + 1}</div>);
+      }
+    }
+
+    for (let i = 0; i < firstWeekDay; i++) {
+      monthDate.unshift(<div className="empty disabled-date" key={`empty-front-${i}`}>{new Date(date.getFullYear(), date.getMonth(), 0 - i).getDate()}</div>)
+    }
+
+    const lastRowDateLength = monthDate.length % 7;
+
+    for (let i = 0; i < (7 - lastRowDateLength); i++) {
+      monthDate.push(<div className="empty disabled-date" key={`empty-back-${i}`}>{new Date(date.getFullYear(), date.getMonth() + 1, 1 + i).getDate()}</div>)
+    }
+
+    return monthDate;
+  }
+
+
+  return (
+    <div className="date-picker">
+      <div className="header">
+        <Icon icon="chevron-left" onClick={handlePreviousMonth} />
+        <div>{`${date.getFullYear()} 年 ${monthNames[date.getMonth()]}`}</div>
+        <Icon icon="chevron-right" onClick={handleNextMonth} />
+      </div>
+      <div className="days">
+        <div className="day">日</div>
+        <div className="day">一</div>
+        <div className="day">二</div>
+        <div className="day">三</div>
+        <div className="day">四</div>
+        <div className="day">五</div>
+        <div className="day">六</div>
+        {renderDate()}
+
+      </div>
+    </div>
+  );
+};
+
+export default DatePicker;
